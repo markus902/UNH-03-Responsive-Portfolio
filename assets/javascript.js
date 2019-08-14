89e719785bc4aecef4bc750c2e61562026739b42
Markus Markowich <markus902@gmx.de>
2019-08-13 22:23:57 -0400

    added links to github for projects

---
+++ assets/javascript.js
@@ -0,0 +1,19 @@
+$(".portfolio").on("click", () => {
+    location.href = 'https://markus902.github.io/UNH-03-Responsive-Portfolio/'
+})
+
+$(".wordGuessGame").on("click", () => {
+    location.href = 'https://markus902.github.io/UNH-04-Word-Guess-Game/'
+})
+
+$(".starWarsRPG").on("click", () => {
+    location.href = 'https://markus902.github.io/UNH-05-Game/'
+})
+
+$(".trivia").on("click", () => {
+    location.href = 'https://markus902.github.io/UNH-06-Trivia/'
+})
+
+$(".rockPaperScissors").on("click", () => {
+    location.href = 'https://markus902.github.io/UNH-08-Rock-Paper-Scissors/'
+})
